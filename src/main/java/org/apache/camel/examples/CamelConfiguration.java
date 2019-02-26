0fbc74933962270cc0d5628ae16d9852e1bd7972
Josh Reagan <jreagan@redhat.com>
2019-02-25 17:18:26 -0700

    cleaned up some typos

--- src/main/java/org/apache/camel/examples/CamelConfiguration.java
+++ src/main/java/org/apache/camel/examples/CamelConfiguration.java
@@ -27,7 +27,6 @@ import org.apache.camel.spi.ComponentCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
 
 @Component
